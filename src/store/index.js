46a066734f70eb241f13f0f4641d7ffcb63020d2
Jonathan Rodriguez <jrodriguez.5716@gmail.com>
2020-11-07 17:20:28 +0100

    refactor: using modules to manage store

--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import homePageActions from '../pages/Home/store';
-import aboutPageActions from '../pages/About/store';
-
-const actions = {
-  ...homePageActions.actions,
-  ...aboutPageActions.actions,
-};
-
-const state = {
-  ...homePageActions.state,
-  ...aboutPageActions.state,
-};
-
-const mutations = {
-  ...homePageActions.mutations,
-  ...aboutPageActions.mutations,
-};
+import homePageStore from '../pages/Home/store';
+import aboutPageStore from '../pages/About/store';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state,
-  mutations,
-  actions,
-  modules: {},
+  modules: {
+    homePageStore,
+    aboutPageStore,
+  },
 });
