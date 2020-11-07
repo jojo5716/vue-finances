08f7f2ffa021a8928dd16c1515274255e20d7323
Jonathan Rodriguez <jrodriguez.5716@gmail.com>
2020-11-07 17:40:51 +0100

    split app into fiels and implementing material design

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
 
 import './registerServiceWorker';
 import App from './App.vue';
@@ -8,7 +11,17 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueMaterial);
 Vue.use(Vuex);
+
+Vue.material = {
+  ...Vue.material,
+  locale: {
+    ...Vue.material.locale,
+    dateFormat: 'dd/MM/yyyy',
+    firstDayOfAWeek: 1,
+  },
+};
 
 new Vue({
   router,
